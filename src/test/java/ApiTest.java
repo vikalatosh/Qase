a16759d5cd048ebb6e217b01a49db175aeba82f3
vikalatosh <latosh.viktorija@mail.ru>
2021-06-14 14:45:27 +0300

    Added the second test into ApiTest (for deleting Project)

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -17,4 +17,28 @@ public class ApiTest {
                 statusCode(200).
                 body("errorMessage", equalTo("Data is invalid"));
     }
+
+    @Test
+    public void projectShouldBeDeleted() {
+        String title = "ProjectTest";
+        String code = "PROJECT";
+        given().
+                header("Token", "833f6af6f92e76d0f3c9b9af42f3c3f176a1950c").
+                header("Content-Type", "application/json").
+                body("{\"title\": \"" + title + "\", \"code\": \"" + code + "\"}").
+        when().
+                post("https://api.qase.io/v1/project").
+        then().
+                statusCode(200).
+                body("status", equalTo(true));
+//                body("result/code", equalTo(code));
+        given().
+                header("Token", "833f6af6f92e76d0f3c9b9af42f3c3f176a1950c").
+                header("Content-Type", "application/json").
+        when().
+                delete("https://api.qase.io/v1/project/PROJECT").
+        then().
+                statusCode(200).
+                body("status", equalTo(true));
+    }
 }
