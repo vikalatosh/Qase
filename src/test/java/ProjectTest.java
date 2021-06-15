9c5e684ea3056cbe2746ccd3f587a46c7ddd3e22
vikalatosh <latosh.viktorija@mail.ru>
2021-06-15 23:02:18 +0300

    Removed tests for updating and deleting project

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -17,47 +17,4 @@ public class ProjectTest extends BaseTest {
         projectsPage.openProjectsPage();
         projectsPage.checkProject(project);
     }
-
-    @Test
-    public void deleteProject() {
-        loginPage.openLoginPage();
-        loginPage.isPageOpened();
-        loginPage.login(user, password);
-        projectsPage.isPageOpened();
-        projectsPage.clickButtonCreateNewProject();
-        projectListPage.isPageOpened();
-        Project project = ProjectFactory.get();
-        projectListPage.createProject(project);
-        projectsPage.openProjectsPage();
-        projectsPage.checkProject(project);
-        projectsPage.openProjectDetailsPage(project);
-        projectDetailsPage.isPageOpened();
-        projectDetailsPage.openSettings();
-        projectSettingsPage.isPageOpened();
-        projectSettingsPage.clickDeleteProject();
-        projectSettingsPage.clickDeleteProject();
-        projectsPage.isPageOpened();
-        projectsPage.checkProjectIsDeleted(project);
-    }
-
-    @Test
-    public void updateProject() {
-        loginPage.openLoginPage();
-        loginPage.isPageOpened();
-        loginPage.login(user, password);
-        projectsPage.isPageOpened();
-        projectsPage.clickButtonCreateNewProject();
-        projectListPage.isPageOpened();
-        Project project = ProjectFactory.get();
-        projectListPage.createProject(project);
-        projectsPage.openProjectsPage();
-        projectsPage.checkProject(project);
-        projectsPage.openProjectDetailsPage(project);
-        projectDetailsPage.isPageOpened();
-        projectDetailsPage.openSettings();
-        projectSettingsPage.isPageOpened();
-        projectSettingsPage.changeProjectData(project);
-        projectSettingsPage.clickUpdateSettings();
-        projectSettingsPage.projectIsUpdated();
-    }
 }
